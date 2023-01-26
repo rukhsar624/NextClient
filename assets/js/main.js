f95f2defb7c46f41d06ad2b955618702689bb23c
rukhsar624 <rukhsaranwar56@gmail.com>
2023-01-26 09:57:22 +0500

    add all files

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+// scroll navbar
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+	if (scroll > 100) {
+		$("#navbar-fixed-top").css("background", "#FFEEEF");
+		$("#navbar-fixed-top").css("box-shadow", "rgb(0 0 0 / 46%) 8px 8px 33px")
+	}
+	else {
+		$("#navbar-fixed-top").css("background", "none");
+		$("#navbar-fixed-top").css("box-shadow", "none")
+
+	}
+})
+// scroll navbar
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+	if (scroll > 100) {
+		$("#register-form").css("background", "#000000");
+		$("#register-form").css("box-shadow", "rgb(0 0 0 / 46%) 8px 8px 33px")
+	}
+	else {
+		$("#register-form").css("background", "none");
+		$("#register-form").css("box-shadow", "none")
+
+	}
+})
+// MOBILE SCREEN SIDE NAV
+var sidebarBox = document.querySelector('#box');
+var sidebarBtn = document.querySelector('#btn');
+var pageWrapper = document.querySelector('#main-content');
+
+sidebarBtn.addEventListener('click', function (event) {
+
+	if (this.classList.contains('active')) {
+		this.classList.remove('active');
+		sidebarBox.classList.remove('active');
+	} else {
+		this.classList.add('active');
+		sidebarBox.classList.add('active');
+	}
+});
+
+pageWrapper.addEventListener('click', function (event) {
+
+	if (sidebarBox.classList.contains('active')) {
+		sidebarBtn.classList.remove('active');
+		sidebarBox.classList.remove('active');
+	}
+});
+
+window.addEventListener('keydown', function (event) {
+
+	if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+		sidebarBtn.classList.remove('active');
+		sidebarBox.classList.remove('active');
+	}
+});
